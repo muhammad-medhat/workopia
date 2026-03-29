e9c297909902bd688a3ada69454b6d9e5246f27d
muhammad medhat <muhammad.medhat@gmail.com>
2026-03-29 23:49:21 +0200

    feat: update dependencies and add TestController with test route
    - Updated devDependencies in package.json to latest versions.
    - Added TestController with resource methods and a test method for handling requests.
    - Created a new test view to display a simple testing message.
    - Added routes for testing functionality in web.php.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 use \Illuminate\Http\Request;
 
@@ -13,3 +14,8 @@ Route::get('/jobs', [JobController::class, 'index']);
 Route::post('/jobs', [JobController::class, 'store']);
 Route::get('/jobs/create', [JobController::class, 'create']);
 Route::get('/jobs/{id}', [JobController::class, 'show']);
+
+// testing requests
+Route::get('/test', [TestController::class, 'index']);
+Route::resource('test', TestController::class);
+Route::get('/test/test', [TestController::class, 'test']);
